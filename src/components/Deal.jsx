e97bcae0b86b5f9be191b2c8259f6860a6f589ff
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2025-03-19 18:24:35 +0100

    style changes

--- src/components/Deal.jsx
+++ src/components/Deal.jsx
@@ -17,25 +17,22 @@ function Deal({dealID, title, ratingText, thumb, savings, salePrice, originalPri
 
             
             <div className="cardContent">
-                <img src={newThumb} alt={title} onError={(e) => e.target.src = thumb} className="thumb" loading="lazy"  />
-                <div className="textContent">
+                <div className="imgContainer">
+                    <img src={newThumb} alt={title} onError={(e) => e.target.src = thumb} className="thumb" loading="lazy"  />
                     <h2 className="marginTop title">{title}</h2>
-
+                </div>
+                <div className="textContent">
+                    <div>
+                        <p>Steam Rating:  <span className="accentColor">{ratingText ? ratingText : "Not Available"}</span></p>
+                        <p>Rating Count: <span className="accentColor">{ratingCount}</span></p>
+                    </div>
                 <div className="idkContainer">
-                    <div>
-                        <p>Steam Rating:  </p>
-                        <p>Rating Count: </p>
-                    </div>
-                    <div>
-                        <p className="accentColor">{ratingText ? ratingText : "Not Available"}</p>
-                        <p className="accentColor">{ratingCount}</p>
-                    </div>
                     <div className="tag">
                         <h2 className="saleTag">-{Math.trunc(savings)}%</h2>
                         <h3 className="priceTag">{salePrice}$</h3>
                         <h5 className="originalPrice">{originalPrice}$</h5>
                     </div>
-                    <img className="logo" src={"https://www.cheapshark.com"+storeLogo} alt="store" />
+                    {/* <img className="logo" src={"https://www.cheapshark.com"+storeLogo} alt="store" /> */}
                 </div>
 
 
