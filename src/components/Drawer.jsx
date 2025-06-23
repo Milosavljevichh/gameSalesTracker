26c6ba0a1efb4948f394e4474c921cf03111d7b2
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2025-06-23 16:20:13 +0200

    added drawer

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,9 +1,48 @@
 import '../styles/drawer.css'
 
 export default function Drawer(){
+    const items = [
+        {
+            name:'General',
+            sectionItems: [
+                {
+                    label:'Theme'
+                },
+                {
+                    label:'Toggle Sale'
+                },
+            ]
+        },
+        {
+            name:'Filter By',
+            sectionItems: [
+                {
+                    label:'Sale %'
+                },
+                {
+                    label:'Price'
+                },
+                {
+                    label:'Reviews'
+                },
+            ]
+        }
+    ]
+    
     return(
         <aside id="drawer">
-            
+            {items.map(item=>(
+                <ul>
+                    <h4>{item.name}</h4>
+                    {item.sectionItems.map(button=>(
+                        <li>
+                            <button>
+                                {button.label}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            ))}
         </aside>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,12 +13,12 @@ function Header({ changeFilter, handleSearch, isDeal, toggleTheme }) {
         <h1>Sales Tracker</h1>
         <SearchBar handleSearch={handleSearch} />
         <div className="flexContainer">
-          <button className="mediumFont" onClick={toggleTheme}>
+          {/* <button className="mediumFont" onClick={toggleTheme}>
             Toggle Theme
-          </button>
-          <button disabled={!isDeal} className="mediumFont" onClick={() => setShowFilters(!showFilters)}>
+          </button> */}
+          {/* <button disabled={!isDeal} className="mediumFont" onClick={() => setShowFilters(!showFilters)}>
             Filter
-          </button>
+          </button> */}
           <div>
             <input disabled={!isDeal} type="checkbox" name="onSale" id="onSale" defaultChecked onChange={() => changeFilter("sale")} />
             <label className="mediumFont" htmlFor="onSale">On Sale</label>
